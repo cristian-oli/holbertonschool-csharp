b7dc42209a9db57bd677645d5eb28211b3a83c8a
Cristian Oliveros <wolfmanoliveros@gmail.com>
2022-04-11 12:16:46 -0500

    added gitignore file, added 0x00-csharp-hello_world Folder, [CREATED] all tasks and the avanced 100

---
+++ 0x00-csharp-hello_world/6-print_string/6-print_string.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		string str = "Holberton School";
+		Console.WriteLine("{0}{1}{2}\n{3}", str, str, str, str.Substring(0,9));
+    }
+}
