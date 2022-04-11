9a5bce1ec9ecd941e0321fa802cdd4f57e03cc6f
Cristian Oliveros <wolfmanoliveros@gmail.com>
2022-04-11 12:52:12 -0500

    [UPDATED] Added namespace

--- 0x00-csharp-hello_world/3-writeline/3-writeline.cs
+++ 0x00-csharp-hello_world/3-writeline/3-writeline.cs
@@ -1,5 +1,10 @@
-﻿class Program {
-    static void Main(string[] args) {
-        Console.WriteLine("\"Programming is like building a multilingual puzzle");
+﻿using System;
+
+namespace _3_writeline
+{
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("\"Programming is like building a multilingual puzzle");
+        }
     }
 }
