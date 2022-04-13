aa063dc25d113d50893fe7efbedb685ef6688fed
Cristian Oliveros <wolfmanoliveros@gmail.com>
2022-04-13 16:06:42 -0500

    [FIXED] Added conditional if index is out of range

--- 0x02-csharp-arrays_lists_dictionaries/2-replace_element/2-replace_element.cs
+++ 0x02-csharp-arrays_lists_dictionaries/2-replace_element/2-replace_element.cs
@@ -4,6 +4,8 @@ class Array
 {
     public static int[] ReplaceElement(int[] array, int index, int n)
     {
+        if (index > array.Length)
+            return array;
         for (int i = 0; i < array.Length; i++)
         {
             if (i == index)
