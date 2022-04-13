1d461e310c8da68acace5222b6dd22c7d04185be
Cristian Oliveros <wolfmanoliveros@gmail.com>
2022-04-13 16:12:10 -0500

    [FIXED] Added string

--- 0x02-csharp-arrays_lists_dictionaries/2-replace_element/2-replace_element.cs
+++ 0x02-csharp-arrays_lists_dictionaries/2-replace_element/2-replace_element.cs
@@ -4,8 +4,12 @@ class Array
 {
     public static int[] ReplaceElement(int[] array, int index, int n)
     {
-        if (index > array.Length)
+        if (index > array.Length || index < 0)
+        {
+            Console.WriteLine("Index out of range");
             return array;
+        }
+
         for (int i = 0; i < array.Length; i++)
         {
             if (i == index)
