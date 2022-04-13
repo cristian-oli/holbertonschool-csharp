d33a33c28526309f4bf67d94547c5f44c5522902
Cristian Oliveros <wolfmanoliveros@gmail.com>
2022-04-13 16:47:13 -0500

    [ADDED] Loop for verify if the key exist

--- 0x02-csharp-arrays_lists_dictionaries/9-add_key_value/9-add_key_value.cs
+++ 0x02-csharp-arrays_lists_dictionaries/9-add_key_value/9-add_key_value.cs
@@ -5,7 +5,13 @@ class Dictionary
 {
     public static Dictionary<string, string> AddKeyValue(Dictionary<string, string> myDict, string key, string value)
     {
-        if (myDict.ContainsKey(key))
+        bool tag = false;
+        foreach ((string itemKey, string itemValue) in myDict)
+        {
+            if (key == itemValue)
+                tag = true;
+        }
+        if (tag)
             myDict[key] = value;
         else
             myDict.Add(key, value);
