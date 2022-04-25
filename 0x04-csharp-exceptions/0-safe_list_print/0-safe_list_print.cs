1874ab66398a064255b4e0f65011c08fc57e8dbb
Cristian Oliveros <wolfmanoliveros@gmail.com>
2022-04-25 01:38:53 -0500

    [CREATED] Added allt ask 0 - 4

---
+++ 0x04-csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int SafePrint(List<int> myList, int n)
+    {
+        int i = 0, prints = 0;
+
+        while (i < n)
+        {
+            try
+            {
+                Console.WriteLine(myList[i]);
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                i++;
+                continue;
+            }
+            prints++;
+            i++;
+        }
+        
+        return prints;
+    }
+}
---
+++ 0x04-csharp-exceptions/2-divide_lists/2-divide_lists.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> Divide(List<int> list1, List<int> list2, int listLength)
+    {
+        List<int> results = new List<int> ();
+
+        for (int i = 0; i < (list1.Count >= list2.Count ? list1.Count : list2.Count); i++)
+        {
+            try
+            {
+                results.Add(list1[i] / list2[i]);
+            }
+            catch (DivideByZeroException)
+            {
+                results.Add(0);
+                Console.WriteLine("Cannot divide by zero");
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.WriteLine("Out of range");
+            }
+        }
+        return results;
+    }
+}
